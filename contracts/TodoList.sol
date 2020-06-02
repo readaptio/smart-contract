f7a1fc9204f887f086bde2589e7af64beda7761d
Vishwas Nahar <vshwsnahar3@gmail.com>
2020-06-02 20:04:03 +0530

    Added Todo migration

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,21 @@ pragma solidity 0.5.16;
 
 contract TodoList {
     uint public taskCount = 0;
+
+    struct Task{
+        uint id;
+        string content;
+        bool is_completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("My First Program in solidity");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
